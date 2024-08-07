52e54ed290d76d36a7accc6c9fda2af0e7262dcc
Malte Münch <muench@b1-systems.de>
2024-08-07 12:07:43 +0200

    Add github action cronjob
    Signed-off-by: Malte Münch <muench@b1-systems.de>

--- cronjob.sh
+++ cronjob.sh
@@ -8,10 +8,12 @@ END_SECONDS=$(date -d "$TODAY" +%s)
 
 GARDENLINUX_VERSION=$(( (END_SECONDS - START_SECONDS) / 86400 ))
 
-cd package_aggregator
-go run . -o ../packages/$GARDENLINUX_VERSION.json
-popd
+cd package_aggregator || exit 1
+go run . -o ../packages/$GARDENLINUX_VERSION.json -exclude package-build
+popd || exit 2
 
+git config --global user.email "package_aggregator@gardenlinux.io"
+git config --global user.name "package_aggregator"
 git add packages/$GARDENLINUX_VERSION.json
 git commit -m "Package status for Gardenlinux Day $GARDENLINUX_VERSION"
 git push -u origin main
